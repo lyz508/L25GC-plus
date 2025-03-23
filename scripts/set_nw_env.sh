8a5022ad1987c632729967c3634dd2b6cae97431
lyz508 <linyangzhe0508@gmail.com>
2025-03-23 18:19:40 +0800

    chore: NW settings on FABRIC

--- scripts/set_nw_env.sh
+++ scripts/set_nw_env.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/bash 
 # UE/AN
-export UEIF='ens1f0'
+export UEIF='enp7s0'
 export UEIF_IP='192.168.1.1'
 export UEIF_IP_ORIGIN='192.168.1.1'
 export UEIF_SUBNET='192.168.1.0'
 # DN
-export DNIF='ens1f1'
+export DNIF='enp7s0'
 export DNIF_IP='10.10.1.2'
 export DNIF_IP_ORIGIN='192.168.1.4'
 export DNIF_SUBNET='10.10.1.0'
 # CN
-export CNANIF='ens1f0'
+export CNANIF='enp9s0'
 export CNANIF_IP='192.168.1.2'
-export CNANIF_MAC='3c:fd:fe:b4:ff:40'
+export CNANIF_MAC='1a:96:c8:13:26:42'
 export CNANIF_ORIGIN_IP='192.168.1.2'
-export CNDNIF='ens1f1'
+export CNDNIF='enp8s0'
 export CNDNIF_IP='10.10.1.1'
-export CNDNIF_MAC='3c:fd:fe:b4:ff:41'
+export CNDNIF_MAC='12:72:97:90:f1:56'
 export CNDNIF_ORIGIN_IP='192.168.1.3'
 # 5G constants
 export UPF_IP='192.168.1.2'
